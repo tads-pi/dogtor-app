b85f23929b770efa7c7b1bcea7b72e3fc19b47f4
kcalixto <kauacalixtocontato@gmail.com>
2023-03-29 17:48:05 -0300

    test ci cd deploy

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello dogtor team!</Text>
+      <Text>Teste ci cd deploy!</Text>
       <StatusBar style="auto" />
     </View>
   );
