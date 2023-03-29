43b8482c9349a108a0802d9c50611bd4c63a4f63
kcalixto <kauacalixtocontato@gmail.com>
2023-03-29 17:54:36 -0300

    final test

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Teste ci cd deploy!</Text>
+      <Text>Aloha!</Text>
       <StatusBar style="auto" />
     </View>
   );
