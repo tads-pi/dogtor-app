c4aed9d7c4a23cf0c5c4b370c61bcbd54d76103a
kcalixto <kauacalixtocontato@gmail.com>
2023-04-05 20:57:05 -0300

    tela de login

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { TelaLogin } from './Pages/telaLogin';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aloha!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <TelaLogin/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
